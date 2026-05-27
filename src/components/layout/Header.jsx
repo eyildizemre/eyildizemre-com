3f0ad59166a0f46d5e71aa3c87af870485bfc4b6
vii7 <eyildiz-emre@outlook.com>
2026-05-27 16:22:44 +0200

    feat: implement Header with routing, theme toggle, and i18n

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,5 +1,28 @@
+import { NavLink, Link } from "react-router-dom";
+import { usePreferences } from "../../context/PreferencesContext";
+import { useTranslation } from "react-i18next";
+
 export function Header({ onToggleSidebar }) {
+    const { theme, setTheme } = usePreferences();
+    const { t } = useTranslation();
+
     return (
-        <header>Header</header>
+        <header>
+            <Link to="/">ey</Link>
+            <nav>
+                <NavLink to="/works">{t("nav.works")}</NavLink>
+                <NavLink to="/code">{t("nav.code")}</NavLink>
+                <NavLink to="/about">{t("nav.about")}</NavLink>
+            </nav>
+
+            <div>
+                <button onClick={() => setTheme(theme === "dark" ? "sepia" : "dark")}>
+                    {theme === "dark" ? t("header.theme.toSepia") : t("header.theme.toDark")}
+                </button>
+                <button onClick={onToggleSidebar} >
+
+                </button>
+            </div>
+        </header>
     );
 }
